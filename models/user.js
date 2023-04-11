60dbe0e65fb326fb06c2386c89a55779a81556ac
leo-moon <49075905+leo-moon@users.noreply.github.com>
2023-04-11 16:33:24 +0300

    create users schemas

---
+++ models/user.js
@@ -0,0 +1,63 @@
+// const { Schema, model } = require("mongoose");
+const { Schema, model } = require("mongoose");
+
+const Joi = require("joi");
+
+const handleMongooseError = require("../utils/handleMongooseError");
+
+// const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const passMinLength = 6;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      unque: true,
+      require: true,
+    },
+    password: {
+      type: String,
+      minlength: passMinLength,
+      require: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const registreSchema = Joi.object({
+  name: Joi.string().required().messages({
+    "any.required": `"name" is required `,
+  }),
+  email: Joi.string().pattern(emailRegexp).messages({
+    "any.required": `"email" is required `,
+  }),
+  password: Joi.string().min(passMinLength).required().messages({
+    "any.required": `"password" is required `,
+  }),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).messages({
+    "any.required": `"email" is required `,
+  }),
+  password: Joi.string().min(passMinLength).required().messages({
+    "any.required": `"password" is required `,
+  }),
+});
+
+const schemas = {
+  registreSchema,
+  loginSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = { User, schemas };
